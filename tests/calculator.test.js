a46d1b766d6906eccace4e19b1eebc507d1a1f00
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-02 10:37:15 -0600

    Se crearon las pruebas unitarias

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,30 @@
 const { default: expect } = require('expect');
+const { multiply } = require('../calculator');
 
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
     //
+    expect(calculator.sum(4,2)).toBe(6);
 })
 
 test('substracts a - b', () => {
    //
+   expect(calculator.substract(4,2)).toBe(2);
 })
 
 test('multiplies a * b', () => {
     //
+    expect(calculator.multiply(4,2)).toBe(8);
 })
 
 test('divides a / b', () => {
     // Consider decimals
+    expect(calculator.divide(4,4)).toBe(1);
 })
 
 test('Raises to power a ^ b', () => {
     //
+    expect(calculator.power(4,2)).toBe(16);
 })
 
