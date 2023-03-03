30d0201ee2946e80f285d7458fbe78a5c7238473
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-03 00:02:05 -0600

    Función para obtener si el número es divisor entre 3, 5 o ambos

---
+++ fizzBuzz.js
@@ -0,0 +1,18 @@
+// Write a function that takes in a number and returns one of the following:
+
+// If the number is divisible by 3, return 'Fizz'.
+// If the number is divisible by 5, return 'Buzz'.
+// If the number is divisible by 3 and 5, return 'FizzBuzz'
+// Else, return the number passed as an argument
+let divisor1 = (num) => {
+    if (num % 3 == 0 && num % 5 == 0) {
+        return console.log('FizzBuzz');
+    } else if (num % 3 == 0) {
+        return console.log ('Fizz');
+    } else if (num % 5 == 0) {
+        return console.log ('Buzz');
+    } else
+        return console.log(num);
+}
+
+divisor1(15);
