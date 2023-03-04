950ff953035b900bbbd9fb57a3fb7512ea041a35
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-03 23:59:18 -0600

    Actualización del código

--- factorial.js
+++ factorial.js
@@ -1,13 +1,26 @@
 // Calculate the factorial of a number using recursion
 
-function factorial (numero) {
-    if(numero ===0 || numero === 1) {
-        return 1;
-    } else {
-        return numero*factorial(numero - 1);
+// function factorial (numero) {
+//     if(numero ===0 || numero === 1) {
+//         return 1;
+//     } else {
+//         return numero*factorial(numero - 1);
         
-    }
+//     }
     
+// }
+
+const factorial = {
+    fact0_1(num){
+        if(num ===0 || num === 1) {
+            return 1;
+        }
+    },
+    factor(num){
+        if(num != 1 && num!=0)
+        return num*factor(num - 1);
+    }
 }
 
-console.log(factorial(5));
+module.exports = factorial;
+// console.log(factorial(4));
