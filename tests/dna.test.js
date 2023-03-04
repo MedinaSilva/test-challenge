ec5a9c65ce39839af3fca383ba432261f69e1263
MedinaSilva <k.medinasilva1234@gmail.com>
2023-03-04 00:07:52 -0600

    Pruebas unitarias para código de DNA

---
+++ tests/dna.test.js
@@ -0,0 +1,5 @@
+//De verdad, estoy haciendo mi mejor y mayor esfuerzo para resolverlo, pero aún no lo consigo
+
+const { default: expect } = require('expect');
+// const { dna} = require('../dna.js');
+const dna = require('../dna.js');
